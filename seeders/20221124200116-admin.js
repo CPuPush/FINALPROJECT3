d51f99f1e374b16bcb6f6399ff490ac4b9deba6d
faridwijayaaa <faridwijayaaa@gmail.com>
2022-11-25 03:17:03 +0700

    Seeding admin data

---
+++ seeders/20221124200116-admin.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await queryInterface.bulkInsert('Users', [{
+      full_name: 'Admin Ngaco',
+      email: "adminngaco@gmail.com",
+      password: "123456",
+      gender: "male",
+      role: 0,
+      balance: 0,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }, 
+    ], {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
