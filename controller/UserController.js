efdc1137c07fc7ffdc677bab2e5a7b12b7731941
faridwijayaaa <faridwijayaaa@gmail.com>
2022-12-01 22:25:26 +0700

    Regis controller

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,12 +1,51 @@
-class UserController{
+const { User } = require("../models");
+const { hashPassword, comparePassword } = require("../helper/bcrypt");
+const { generateToken } = require("../helper/jwt");
 
-  static async userRegister(req, res){
+class UserController {
+  static async userRegister(req, res) {
     try {
-      const {full_name, password, gender, email} = req.body;
+      const { full_name, password, gender, email } = req.body;
+
+      if (!password) {
+        return res.status(400).json({ message: "password cannot be empty" });
+      }
+      // const hashedPassword = hashPassword(password);
+      // return res.status(201);
+      const user = await User.create({
+        full_name: full_name,
+        email: email,
+        password: password,
+        gender: gender,
+        role: 1,
+        balance: 0,
+      });
+
+      let balanceFormat = new Intl.NumberFormat("id-ID", {
+        style: "currency",
+        currency: "IDR",
+      }).format(parseInt(user.balance));
+
+      return res.status(201).json({
+        id: user.id,
+        full_name,
+        email,
+        gender,
+        balance: balanceFormat,
+        createdAt: user.createdAt,
+      });
     } catch (error) {
-      return res.json('error');
+      let errorMes = error.name;
+      if (
+        errorMes === "SequelizeUniqueConstraintError" ||
+        errorMes === "SequelizeValidationError"
+      ) {
+        return res.status(400).json({ message: error.errors[0].message });
+      }
+      console.log(error);
+      return res.status(404).json(error);
     }
   }
-
 }
+
 module.exports = UserController;
--- models/user.js
+++ models/user.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -15,86 +13,97 @@ module.exports = (sequelize, DataTypes) => {
       this.hasMany(models.TransactionHistory);
     }
   }
-  User.init({
-    full_name: {
-      type: DataTypes.STRING,
-      validate: {
-        notEmpty: {
-          args: true,
-          msg: "full_name cannot be empty"
-        }
-      }
-    },
-    email: {
-      type: DataTypes.STRING,
-      unique: true,
-      validate: {
-        isEmail: {
-          args: true,
-          msg: "Please enter valid email format"
+  User.init(
+    {
+      full_name: {
+        type: DataTypes.STRING,
+        validate: {
+          notEmpty: {
+            args: true,
+            msg: "full_name cannot be empty",
+          },
         },
-        notEmpty: {
-          args: true,
-          msg: "email cant be empty"
+      },
+      email: {
+        type: DataTypes.STRING,
+        validate: {
+          isEmail: {
+            args: true,
+            msg: "Please enter valid email format",
+          },
+          notEmpty: {
+            args: true,
+            msg: "email cant be empty",
+          },
         },
-      }
-    },
-    password: {
-      type: DataTypes.STRING,
-      validate: {
-        len: ([6, 10], {
+        unique: {
           args: true,
-          msg: "long password must be between 6 to 10"
-        }),
-        notEmpty: {
-          args: true,
-          msg : "password can't be empty"
-        }
-      }
-    },
-    gender: {
-      type: DataTypes.STRING,
-      validate: {
-        notEmpty: {
-          args: true,
-          msg: "gender cant be empty"
+          msg: "Email already exists",
         },
-        // ! this not fix
-        isIn: ([['male', 'female']], {
-          args: true,
-          msg: "only fill in gender with male or female"
-        })
-      }
-    },
-    role: {
-      type: DataTypes.INTEGER,
-      validate: {
-        notEmpty: {
-          args: true,
-          msg: "Role cant be empty"
+      },
+      password: {
+        type: DataTypes.STRING,
+        validate: {
+          len: {
+            args: [6, 10],
+            msg: "long password must be between 6 to 10",
+          },
+          notEmpty: {
+            args: true,
+            msg: "password can't be empty",
+          },
         },
-        // ! this not fix
-        isIn: ([[0, 1]], {
-          args: true,
-          msg: "only fill in role with admin or customer"
-        }) 
-      }
-    },
-    // ! kurang field username empty
-    balance: {
-      type: DataTypes.INTEGER,
-      validate: {
-        isInt: {
-          args: true,
-          msg: "Balance has integer data type"
+      },
+      gender: {
+        type: DataTypes.STRING,
+        validate: {
+          notEmpty: {
+            args: true,
+            msg: "gender cant be empty",
+          },
+          // ! this not fix
+          isIn: {
+            args: [["male", "female"]],
+            msg: "only fill in gender with male or female",
+          },
         },
-        max: 100000000,
-        min: 0
-      }
+      },
+      role: {
+        type: DataTypes.INTEGER,
+        validate: {
+          notEmpty: {
+            args: true,
+            msg: "Role cant be empty",
+          },
+          // ! this not fix
+          isIn: {
+            args: [[0, 1]],
+            msg: "only fill in role with admin or customer",
+          },
+        },
+      },
+      // ! kurang field username empty
+      balance: {
+        type: DataTypes.INTEGER,
+        validate: {
+          isInt: {
+            args: true,
+            msg: "Balance has integer data type",
+          },
+          max: 100000000,
+          min: 0,
+        },
+      },
+    },
+    {
+      hooks: {
+        beforeValidate: (user, options) => {
+          user.balance = 0;
+        },
+      },
+      sequelize,
+      modelName: "User",
     }
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
+  );
   return User;
 };
