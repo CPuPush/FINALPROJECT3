bdab29fa5f426dea4f785f598cf1d2f140a3ad37
faridwijayaaa <faridwijayaaa@gmail.com>
2022-12-07 01:59:51 +0700

    Route user & category

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 const router = require("express").Router();
-const UserController = require("../controller/UserController");
 const authentication = require("../middleware/authentication");
+const UserController = require("../controller/UserController");
 const authorizationUser = require("../middleware/authorizationUser");
+const CategoryController = require("../controller/CategoryController");
+const authorizationCategory = require("../middleware/authorizationCategory");
+const authRole = require("../middleware/authRole");
 
+// ! USER
 router.post("/users/register", UserController.userRegister);
 router.post("/users/login", UserController.userLogin);
 router.put(
@@ -18,5 +22,28 @@ router.delete(
   UserController.deleteUser
 );
 router.patch("/users/topup", authentication, UserController.userTopup);
+
+// ! CATEGORY
+router.post(
+  "/categories",
+  authentication,
+  authRole,
+  CategoryController.createCategory
+);
+router.get("/categories", authentication, CategoryController.getAllCategory);
+router.patch(
+  "/categories/:categoryId",
+  authentication,
+  authRole,
+  authorizationCategory,
+  CategoryController.patchCategory
+);
+router.delete(
+  "/categories/:categoryId",
+  authentication,
+  authRole,
+  authorizationCategory,
+  CategoryController.deleteCategory
+);
 
 module.exports = router;
