8626c7025c3e363ae587b7257a98f7115c54e83c
faridwijayaaa <faridwijayaaa@gmail.com>
2022-12-09 20:49:22 +0700

    server.js

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const PORT = process.env.PORT || 3000;
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const PORT = process.env.PORT || 3333;
 
 const app = express();
-const routes = require('./routes');
+const routes = require("./routes");
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended : true }))
+app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 app.listen(PORT, () => {
